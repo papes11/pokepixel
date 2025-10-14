fff11fc519bf4c38acccd1a4f36fb995f3481a0c
papes11 <papesrock11@gmail.com>
2025-10-14 14:25:01 +0530

    updated

--- pages/maintenance.tsx
+++ pages/maintenance.tsx
@@ -1,3 +1,99 @@
+// import React from 'react';
+// import styled from 'styled-components';
+// import Gameboy from '../src/components/Gameboy';
+// import GlobalStyles from '../src/styles/GlobalStyles';
+// import WalletContextProvider from '../src/wallets/wallet-provider';
+// import { Provider } from 'react-redux';
+// import { store } from '../src/state/store';
+// import InstallPrompt from '../src/components/InstallPrompt';
+// import { usePWAInstall } from '../src/hooks/usePWAInstall';
+
+// const StyledApp = styled.div`
+//   background: black;
+//   width: 100vw;
+//   height: 100dvh;
+//   border-radius: 20px;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   padding: 2px;
+//   padding-bottom: 25px;
+
+//   @media (min-width: 1000px) {
+//     padding: 5px;
+//   }
+// `;
+
+// const MaintenanceScreen = styled.div`
+//   width: 100%;
+//   height: 100%;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+//   background:rgb(10, 10, 8);
+//   font-family: 'Press Start 2P', monospace;
+//   padding: 20px;
+//   text-align: center;
+// `;
+
+// const MaintenanceText = styled.div`
+//   color: #0f380f;
+//   font-size: 18px;
+//   font-weight: bold;
+//   line-height: 1.8;
+//   text-shadow: 2px 2px 0px #8bac0f;
+//   margin-bottom: 10px;
+//   font-family: monospace;
+
+//   @media (max-width: 640px) {
+//     font-size: 14px;
+//   }
+// `;
+
+// const AnimatedDots = styled.span`
+//   @keyframes blink {
+//     0%, 100% { opacity: 1; }
+//     50% { opacity: 0; }
+//   }
+//   animation: blink 1.5s infinite;
+// `;
+
+// export default function MaintenancePage() {
+//   // PWA Install functionality
+//   const { showInstallPrompt, installApp, dismissPrompt } = usePWAInstall();
+
+//   return (
+//     <WalletContextProvider>
+//       <Provider store={store}>
+//         <GlobalStyles />
+//         {showInstallPrompt && (
+//           <InstallPrompt 
+//             onInstall={installApp} 
+//             onDismiss={dismissPrompt} 
+//           />
+//         )}
+//         <StyledApp>
+//           <Gameboy>
+//             <MaintenanceScreen>
+//               <MaintenanceText>
+//                 UNDER
+//                 <br />
+//                 MAINTENANCE
+//                 <AnimatedDots>...</AnimatedDots>
+//               </MaintenanceText>
+//               <MaintenanceText style={{ fontSize: '12px', marginTop: '30px' }}>
+//                 WE&apos;LL BE
+//                 <br />
+//                 BACK SOON!
+//               </MaintenanceText>
+//             </MaintenanceScreen>
+//           </Gameboy>
+//         </StyledApp>
+//       </Provider>
+//     </WalletContextProvider>
+//   );
+// }
 import React from 'react';
 import styled from 'styled-components';
 import Gameboy from '../src/components/Gameboy';
@@ -24,20 +120,20 @@ const StyledApp = styled.div`
   }
 `;
 
-const MaintenanceScreen = styled.div`
+const LaunchScreen = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background:rgb(10, 10, 8);
+  background: rgb(10, 10, 8);
   font-family: 'Press Start 2P', monospace;
   padding: 20px;
   text-align: center;
 `;
 
-const MaintenanceText = styled.div`
+const LaunchText = styled.div`
   color: #0f380f;
   font-size: 18px;
   font-weight: bold;
@@ -59,7 +155,7 @@ const AnimatedDots = styled.span`
   animation: blink 1.5s infinite;
 `;
 
-export default function MaintenancePage() {
+export default function LaunchPage() {
   // PWA Install functionality
   const { showInstallPrompt, installApp, dismissPrompt } = usePWAInstall();
 
@@ -75,19 +171,19 @@ export default function MaintenancePage() {
         )}
         <StyledApp>
           <Gameboy>
-            <MaintenanceScreen>
-              <MaintenanceText>
-                UNDER
+            <LaunchScreen>
+              <LaunchText>
+                ALPHANET
                 <br />
-                MAINTENANCE
+                IS LIVE
                 <AnimatedDots>...</AnimatedDots>
-              </MaintenanceText>
-              <MaintenanceText style={{ fontSize: '12px', marginTop: '30px' }}>
-                WE&apos;LL BE
+              </LaunchText>
+              <LaunchText style={{ fontSize: '12px', marginTop: '30px' }}>
+                LAUNCHING
                 <br />
-                BACK SOON!
-              </MaintenanceText>
-            </MaintenanceScreen>
+                SOON!
+              </LaunchText>
+            </LaunchScreen>
           </Gameboy>
         </StyledApp>
       </Provider>
