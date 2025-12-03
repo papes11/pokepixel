2d61d09c71b0502ed6f6e8fb26f226ac72c4579b
papes11 <papesrock11@gmail.com>
2025-12-03 14:45:04 +0530

    updated

--- src/components/ClaimPopup.tsx
+++ src/components/ClaimPopup.tsx
@@ -157,7 +157,7 @@ const ClaimPopup: React.FC<ClaimPopupProps> = ({ isOpen, onClose }) => {
         ) : hasNFT === true ? (
           <div>
             <div className="info-message">
-              Congratulations! You are eligible for the airdrop ~POKEPIXEL 25$ .
+              Congratulations! You are eligible for the airdrop ~25$ POKEPIXEL
             </div>
             <p className="popup-message">
               You own SOLANA GAMING PASS. The airdrop will be distributed to your wallet soon.
